f5a9ecfad69aa85e47dd61263b402c0fcc3d58a6
Nonnorso <r.philippot@live.fr>
2023-09-26 10:34:41 +0200

    correction de l'affichage du slider, il n'y a plus d'image vide. correction des problemes liés à la key unique et de la condition sur le tableau est 'null' ou undefinied

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,48 +9,48 @@ const Slider = () => {
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+  ) || [];
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex((index + 1) % byDateDesc.length),
       5000
     );
   };
   useEffect(() => {
     nextCard();
-  });
+  }, [index, byDateDesc]);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.id}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
+                /* eslint-disable react/no-array-index-key */
                 <input
-                  key={`${event.id}`}
+                  key={`${event.id}-${radioIdx}`}
                   type="radio"
                   name="radio-button"
                   checked={idx === radioIdx}
                 />
+                /* eslint-enable react/no-array-index-key */
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
